e0e4c6399711345dce92cb3d8f53b43594cf04b9
lat0o <lars_erik@live.no>
2021-03-18 13:10:34 +0100

    animation cont

--- app.js
+++ app.js
@@ -6,6 +6,15 @@
     }
 }) */
 
+const gra = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
+const init = function(){
+  //let items = document.querySelectorAll('section');
+  //cssScrollSnapPolyfill()
+}
+init();
+
 
 function getScrollPercent() {
     var h = document.documentElement, 
@@ -15,13 +24,14 @@ function getScrollPercent() {
     return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100
 };
 
+const p2 = document.getElementById('#p2');
+
 const tl = anime.timeline({ autoplay: false });
 tl
 .add({
     targets: '#p1',
     keyframes: [
-        {translateX: -100, opacity: 0, duration:0},
-        {translateX: 0, opacity:1, duration:5, translateY: 250, duration:5},
+        { opacity:1, duration:0},
         {translateX: 50, opacity:0, delay: 0, duration:100}
     ],
     //autoplay: false,
@@ -34,8 +44,8 @@ tl
     
     targets: '#p2-text',
     keyframes: [
-        {translateX: -200, opacity: 0, duration:0},
-        {translateX: 0, opacity:1, duration:5, translateY: 250, duration:5},
+        {translateX: -400, opacity: 0, duration:0},
+        {translateX: 0, opacity:1, duration:5, translateY: 0, duration:5},
         {opacity: 0, duration: 2}
     ],
     //autoplay: false,
@@ -46,7 +56,7 @@ tl
     targets: '#p2-img',
     keyframes: [
         {translateY: 400, opacity: 0, duration: 0},
-        {translateY: 250, opacity: 1, duration: 5},
+        {translateY: 0, opacity: 1, duration: 5},
         {opacity: 0, duration: 2}
     ],
     //autoplay: false,
@@ -54,17 +64,64 @@ tl
     delay: anime.stagger(2500, {start: 5}),
 }, 0)
 .add ({
-    targets: '#p3',
+    targets: '#p3-text',
     keyframes: [
-        {opacity: 0, duration:0},
-        {opacity: 1, duration: 5}
+        {translateX: 400, translateY: -350, opacity: 0, duration:0},
+        {translateX: -200, translateY: 300, opacity: 1, duration: 4},
+        {translateY: -400, opacity: 0, duration: 30}
     ],
     //autoplay: false,
     easing: 'linear',
-    delay: anime.stagger(2500, {start: 10}),
+    delay: anime.stagger(2500, {start: 12}),
+}, 0)
+.add({
+    targets: '#p3-img',
+    keyframes: [
+        {translateX: -400, translateY: 350, opacity: 0, duration:0},
+        {translateX: 300, translateY: -100, opacity: 1, duration: 4},
+        {opacity: 0, duration: 7}
+    ],
+    easing: 'linear',
+    delay: anime.stagger(2500, {start: 12}),
+}, 0)
+.add({
+    targets: '#p4-text',
+    keyframes: [
+        {translateX: -800, opacity: 0, duration:0},
+        {translateX: -150, translateY: 300, opacity: 1, duration: 5},
+    ],
+    easing: 'linear',
+    delay: anime.stagger(2500, {start: 20}),
+}, 0)
+.add({
+    targets: '#p4-img',
+    keyframes: [
+        {translateX: 250, translateY: -100, opacity: 1, duration: 0},
+    ],
+    easing: 'linear',
+    delay: anime.stagger(2500, {start: 20}),
+}, 0)
+.add({
+    targets: '#p5-text',
+    keyframes: [
+        {translateX: -150, translateY: 300, opacity: 1, duration:0},
+    ],
+    easing: 'linear',
+    delay: anime.stagger(2500, {start: 25}),
+}, 0)
+.add({
+    targets: '#p5-text2',
+    keyframes: [
+        {translateX: -800, opacity: 0, duration:0},
+        {translateX: -150, translateY: 300, opacity: 1, duration: 8},
+    ],
+    easing: 'linear',
+    delay: anime.stagger(2500, {start: 25}),
 }, 0)
 
-/* var text = anime({
+
+
+ /* var text = anime({
     targets: '#p2 p',
     keyframes: [
         {translateX: -100, opacity: 0, duration:0},
@@ -77,7 +134,7 @@ tl
 window.addEventListener('scroll', () => {
 	const persentage = getScrollPercent()
 	tl.seek(tl.duration * (persentage / 100))
-})
+});
 
 
  
